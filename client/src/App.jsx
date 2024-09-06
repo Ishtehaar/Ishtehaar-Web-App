f9c1945e9075fd9cc606f94fdc8076d6cf258a47
Muhammad Tayyab <tayyabm708@gmail.com>
2024-09-06 20:23:20 +0500

    created initial pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 export default function App() {
   return (
-    <div className='font-bold text-center text-red-400'>App</div>
-  )
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
