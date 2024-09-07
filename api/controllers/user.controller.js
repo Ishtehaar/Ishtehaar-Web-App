5ce7e335f5abc4f9c3c73baf5dd6bec7b0e9114b
Muhammad Tayyab <tayyabm708@gmail.com>
2024-09-07 21:50:32 +0500

    created user model and first test api route as well added custom error handler for all backend errors and also added a middleware function to handle errors

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,7 @@
+
+
+export const test = async (req, res, next) => {
+    res.send("Hello from test");
+  
+};
+
--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -18,4 +20,19 @@ const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes)
+
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
